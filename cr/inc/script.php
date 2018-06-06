79d7818b6d9dad1dc857955809c9b7402fb63391
DESKTOP-20R18M5\Ariel Calli <arielcallisaya00@gmail.com>
2018-06-06 17:42:55 -0300

    no message

--- cr/inc/script.php
+++
@@ -1,8 +0,0 @@
-<script src="library/mdb/js/jquery-3.2.1.min.js"></script>
-<script src="library/mdb/js/popper.min.js"></script>
-<script src="library/mdb/js/bootstrap.js"></script>
-<script src="library/mdb/js/mdb.js"></script>
-<script src="library/alertifyjs/alertify.js"></script>
-<script src="js/main.js"></script>
-
-
