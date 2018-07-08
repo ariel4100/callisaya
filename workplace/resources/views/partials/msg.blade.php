8f87f8e683ac243e0504f8ee92b83967c8baa4b0
DESKTOP-20R18M5\Ariel Calli <arielcallisaya00@gmail.com>
2018-07-08 03:32:44 -0300

    ejercice

--- workplace/resources/views/partials/msg.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-@if(session('successMsg'))
-    <div class="alert alert-success">
-        <button type="button" aria-hidden="true" class="close" onclick="this.parentElement.style.display='none'">×</button>
-        <span>
-            <b> Success - </b> {{ session('successMsg') }}</span>
-    </div>
-@endif
